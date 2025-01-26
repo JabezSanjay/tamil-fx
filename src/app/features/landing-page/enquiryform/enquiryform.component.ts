229a5a1e5cd0ee5150e9ea9763e33fc72e2af5c2
JabezSanjay <jbzsanjay2000@gmail.com>
2025-01-26 11:04:55 +0530

    enhance enquiry form by adding investment field and improving layout

--- src/app/features/landing-page/enquiryform/enquiryform.component.ts
+++ src/app/features/landing-page/enquiryform/enquiryform.component.ts
@@ -1,36 +1,39 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-enquiryform',
+  selector: "app-enquiryform",
   standalone: true,
   imports: [FormsModule, CommonModule],
-  templateUrl: './enquiryform.component.html',
-  styleUrl: './enquiryform.component.scss',
+  templateUrl: "./enquiryform.component.html",
+  styleUrl: "./enquiryform.component.scss",
 })
 export class EnquiryformComponent {
   @Output() close = new EventEmitter<void>(); // Emits an event to close the form
   @Output() submit = new EventEmitter<any>(); // Emits the form data upon submission
   formData = {
-    name: '',
-    email: '',
-    phone: '',
-    tradedBefore: '',
-    broker: '',
+    name: "",
+    email: "",
+    phone: "",
+    tradedBefore: "",
+    broker: "",
+    investment: "",
   };
   // Handle form submission
   submitEnquiry() {
-    console.log('Form Submitted:', this.formData);
+    console.log("Form Submitted:", this.formData);
 
     // Redirect to WhatsApp with the details
     const whatsappMessage = `Name: ${this.formData.name}%0AEmail: ${
       this.formData.email
     }%0APhone: ${this.formData.phone}%0ATraded Before: ${
       this.formData.tradedBefore
-    }%0AWhich Broker: ${this.formData.broker || 'N/A'}`;
+    }%0AWhich Broker: ${
+      this.formData.broker || "N/A"
+    }%0AHow much do you want to invest: ${this.formData.investment}`;
     const whatsappUrl = `https://wa.me/8686823333?text=${whatsappMessage}`;
-    window.open(whatsappUrl, '_blank');
+    window.open(whatsappUrl, "_blank");
   }
 
   closeDialog() {
@@ -38,8 +41,8 @@ export class EnquiryformComponent {
   }
   // Triggered when "Traded Before" value changes
   checkTradedBefore() {
-    if (this.formData.tradedBefore === 'no') {
-      this.formData.broker = ''; // Clear the broker field
+    if (this.formData.tradedBefore === "no") {
+      this.formData.broker = ""; // Clear the broker field
     }
   }
 }
